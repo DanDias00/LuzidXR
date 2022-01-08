704b01ebced6ecaea9ecfc4c1d5ca875e16e0b53
DanDias00 <65335971+DanDias00@users.noreply.github.com>
2022-01-08 20:12:21 +0530

    Stable version of BlazePose

---
+++ newBlaze/blazepose-p5js/src/blazepose.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+let landmarks
+
+const pose = new Holistic({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+}});
+
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: true,
+  smoothSegmentation: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+
+pose.onResults(results => {
+  landmarks = results.poseLandmarks
+
+});
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await pose.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+
+
+camera.start();
