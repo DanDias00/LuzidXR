2ef0abc99116ddaf8d1214471ec6ca50ea32a5f8
DanDias00 <65335971+DanDias00@users.noreply.github.com>
2022-03-19 17:21:28 +0530

    Added final js

---
+++ final changes/js/product.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function () {
+    let url = new URLSearchParams(this.location.search);
+    let id = url.get("productId");
+
+
+//to retrieve the product details
+    for (product of products) {
+        if (product.id == id) {
+
+            this.document.getElementById("p_name").innerHTML += product.name;
+            this.document.getElementById("p_des").innerHTML += product.description;
+            this.document.getElementById("p_price").innerHTML += product.price;
+            this.document.getElementById("myImg").src = product.image;
+
+
+        }
+    }
+}
+
+);
+
+// create method to pass to unity
+function sendToUnity(){
+    let url = new URLSearchParams(this.location.search);
+    let id = url.get("productId");
+    window.location.href = "unity.html?val="+id+"" ;
+
+   
+  }
